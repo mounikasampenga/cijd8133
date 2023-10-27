32d0bdde120259787688c2ce6e9ca21ebc058e9d
mounikasampenga <149147560+mounikasampenga@users.noreply.github.com>
2023-10-27 13:38:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,4 @@
+var parent=[React.createElement("h1",{},"javascript"),React.createElement("p",{},"react javascript")];
+var rootElement=document.getElementById("root");
+var rootCont=ReactDOM.createRoot("rootElement");
+rootCont.render(parent)
